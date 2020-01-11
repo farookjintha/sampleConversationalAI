471da1e10fb3253d47761f00a922bc8bc5c3d8f1
farookjintha <farookjintha@gmail.com>
2020-01-11 11:14:57 +0530

    modified process.env

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,6 @@ module.exports = {
     googleProjectID : process.env.GOOGLE_PROJECT_ID,
     dialogFlowSessionId : process.env.DIALOGFLOW_SESSION_ID,
     dialogFlowSessionLanguageCode : process.env.DIALOGFLOW_LANGUAGE_CODE,
-    googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL ,
+    googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
     googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY
 }
