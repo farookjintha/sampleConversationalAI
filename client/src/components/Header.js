39af40ec11c632c79b6186ff9ef57f43021ed4d2
farookjintha <farookjintha@gmail.com>
2020-01-15 00:24:32 +0530

    chatbot component added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,14 +4,17 @@ import { Link } from 'react-router-dom';
 
 const Header = () => (
         <nav>
-            <ul>
-                <li>
-                    <Link to = {'/shop'}>Shop</Link>
-                </li>
-                <li>
-                    <Link to = {'/about'}>About Us</Link>
-                </li>
-            </ul>
+            <div className="nav-wrapper">
+                <Link to={'/'} className='brand-logo'>Farook Jintha</Link>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li>
+                        <Link to = {'/shop'}>Shop</Link>
+                    </li>
+                    <li>
+                        <Link to = {'/about'}>About Us</Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 
