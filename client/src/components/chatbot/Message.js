16d8156d779146268e083c7c3d31c33094d9780e
farookjintha <farookjintha@gmail.com>
2020-02-26 01:19:36 +0530

    added styles

--- client/src/components/chatbot/Message.js
+++ client/src/components/chatbot/Message.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Message = (props) => {	
   return(<div className={props.speaks === "me" ? "right-align" : ""}>
-    {/* <span className = {props.speaks === "bot" ? "chat_avatar": ""}>
+    <span className = {props.speaks === "bot" ? "chat_avatar": ""}>
           {props.speaks === 'bot' ? <img src={require('../../assets/fj.jpg')}/> : ""}
-          </span> */}
+          </span>
       <div className={`chatbubble ${props.speaks === "bot" ? "bot " : "me"}`}>
                   <span>{props.text}</span>
       </div>
