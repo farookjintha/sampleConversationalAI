6d3e10342b277c381d23108acc4cb96f1d07c53b
Farook Jintha <farookjintha@gmail.com>
2020-03-05 18:18:41 +0530

    styles modified

--- client/src/components/chatbot/BotSummary.js
+++ client/src/components/chatbot/BotSummary.js
@@ -3,12 +3,14 @@ import React from 'react';
 const BotSummary = () =>{
     return(
         <div className = 'show_details chat_converse'>
-            <h2>Bot Summary</h2>
-            <p>
-                This bot is powered by AI to provide better user experience. <br/>
-                The Complete SDLC has been implemented right from designing till CI/CD. 
+            <span id = 'summary-head'>
+                <p>Bot Summary</p>
+            </span>
+            <p id = 'summary-body'>
+                This bot is powered by AI to provide better user experience. <br/><br/>
+                The Complete SDLC has been implemented right from designing till CI/CD. <br/><br/>
                 In order to make this bot a light-weighted one, it uses React for the front-end which is then 
-                integrated with a NLP (Seq2Seq) model with a Flask(backend) server. On the other hand, it has Dialogflow integration as a backup, an NLU agent which is served using Node.js and Express. <br/>
+                integrated with a NLP (Seq2Seq) model with a Flask(backend) server. On the other hand, it has Dialogflow integration as a backup, an NLU agent which is served using Node.js and Express. <br/><br/>
                 This chatbot is progressing every now and then. Feel free to converse with it. <br/>
                 <br/>
                 <br/>
